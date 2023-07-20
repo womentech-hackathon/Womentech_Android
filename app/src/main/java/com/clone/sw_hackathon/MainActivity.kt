9d0988b382324ddec7d970eb244717778bcaf481
jung0115 <jungim7490@gmail.com>
2023-07-21 05:51:15 +0900

    [SETTING] #1 - ViewBinding, Layout 폴더 분류

--- app/src/main/java/com/clone/sw_hackathon/MainActivity.kt
+++ app/src/main/java/com/clone/sw_hackathon/MainActivity.kt
@@ -2,10 +2,22 @@ package com.clone.sw_hackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clone.sw_hackathon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // ViewBinding Setting
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ViewBinding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
